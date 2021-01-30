3d5c7d1c7ebc91f3f10f7f295cdbf745bc782bfd
Canadienwolf <39683148+Canadienwolf@users.noreply.github.com>
2021-01-30 14:29:45 +0100

    Was late night workday

--- SpookyRooms/Assets/_Scripts/InteractableObject.cs
+++ SpookyRooms/Assets/_Scripts/InteractableObject.cs
@@ -1,17 +1,68 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class InteractableObject : MonoBehaviour
 {
     public GameObject pickupText;
+
+    public bool pickedUp;
+    private GameObject handToFollow;
     
     private void OnTriggerStay2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
-            if (Input.GetKey(KeyCode.E))
+            handToFollow = gameObject.;
+            pickupText.gameObject.SetActive(true);
+
+            if (Input.GetKeyDown(KeyCode.E))
             {
-                gameObject.SetActive(false);
+                if (pickedUp)
+                {
+                    pickedUp = false;
+                }
+
+                if (!pickedUp)
+                {
+                    pickedUp = true;
+                }
+
+
             }
+
+            /*
+            if (Input.GetKeyDown(KeyCode.E) && pickedUp);
+            {
+                pickedUp = false;
+
+                StartCoroutine(waitingTime(100f));
+                print("Good vodoo");
+            }
+            */
         }
+    }
+
+    
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            pickupText.gameObject.SetActive(false);
+        }
+    }
+
+    private void Update()
+    {
+        if (pickedUp)
+        {
+            gameObject.transform.position = handToFollow.transform.position;
+        } 
+    }
+
+    //might not be needed?
+    IEnumerator waitingTime(float time)
+    {
+        yield return new WaitForSeconds(time);
     }
 }
